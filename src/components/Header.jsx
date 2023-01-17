4e5a830f472f18f851e87d466ced418336a83f5f
prathmesh-bendal <prathmeshbendal@gmail.com>
2023-01-17 21:05:37 +0530

    dynamic display of profile/signin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from 'react'
 
 import { useLocation ,useNavigate} from 'react-router'
 export default function Header() {
+  const [pageState,setPageState]=useState("Sign In")
   const location=useLocation();
   const navigate=useNavigate();
+  const auth=getAuth();
+  useEffect(()=>{
+    onAuthStateChanged(auth,(user)=>{
+      if(user)
+      {
+        setPageState("Profile")
+      }
+      else{
+        setPageState("Sign In")
+      }
+    })
+  },[auth])
   function pathMapRoute(route)
   {
     if(route === location.pathname)
@@ -24,7 +38,7 @@ export default function Header() {
               <ul className='flex space-x-10'>
                 <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMapRoute("/") && 'text-black border-b-red-500'} `} onClick={()=> navigate("/")}>Home</li>
                 <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMapRoute("/offers") && 'text-black border-b-red-500'} `} onClick={()=> navigate("/offers")}>Offers</li>
-                <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMapRoute("/sign-in") && 'text-black border-b-red-500'} `} onClick={()=> navigate("/sign-in")}>Sign-in</li>
+                <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${(pathMapRoute("/sign-in") || pathMapRoute("/profile")) && 'text-black border-b-red-500'} `} onClick={()=> navigate("/profile")}>{pageState}</li>
               </ul>
             </div>
         </header>
