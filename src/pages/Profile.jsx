a11d32c5b915a9393f0165672706be46943103ba
prathmesh-bendal <prathmeshbendal@gmail.com>
2023-01-17 20:47:51 +0530

    implemented update profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,13 @@
-import { getAuth } from 'firebase/auth'
+import { getAuth, updateProfile } from 'firebase/auth'
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router';
-
+import { toast } from 'react-toastify';
+import { doc, serverTimestamp, setDoc, updateDoc } from 'firebase/firestore';
+import { db } from "../firebase";
 export default function Profile() {
   const auth=getAuth();
   const navigate=useNavigate();
+  const [changeDetail,setChangeDetail]=useState(false)
   const [formData,setFormData]=useState({
     name:auth.currentUser.displayName,
     email:auth.currentUser.email
@@ -16,18 +19,53 @@ export default function Profile() {
     navigate("/")
 
   }
+  function onChange(e)
+  {
+    setFormData((prev)=>({
+      ...prev,
+      [e.target.id]:e.target.value
+    }))
+  }
+  async function onSubmit()
+  {
+    try{
+      if(auth.currentUser.displayName!==name)
+      {
+        await updateProfile(auth.currentUser,{
+          displayName:name
+        })
+
+        const docRef=doc(db,"users",auth.currentUser.uid)
+        await updateDoc(docRef,{
+          name
+        })
+
+        toast.success("Profile Updated")
+      }
+    }
+    catch(error)
+    {
+      toast.error("Could not update Details")
+    }
+  }
   return (
     <>
       <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
         <h1 className='text-3xl text-center mt-6 font-bold'>My Profile</h1>
         <div className='w-full md:w-[50%] mt-6 px-3'>
           <form >
-            <input type='text' id='name' value={name} disabled className='w-full px-16 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6'/>
+            <input type='text' id='name' value={name} disabled={!changeDetail} onChange={onChange} className={`w-full px-16 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6 ${changeDetail && "bg-red-200 focus:bg-red-200"}` }/>
             <input type='email' id='email' value={email} disabled className='w-full px-16 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6'/>
 
             <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6'>
               <p className='flex items-center'>Do you want to change your name?
-                <span className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'>edit</span>
+                <span onClick={()=>{
+                  changeDetail && onSubmit()
+                  setChangeDetail(prev => !prev)
+                }
+                  } className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'>
+                  {changeDetail ? "Apply Change" : "Edit"}
+                </span>
               </p>
               <p onClick={onLogout} className='text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 cursor-pointer'>sign out</p>
             </div>
